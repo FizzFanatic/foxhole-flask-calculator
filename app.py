47b282f90e0afefb5110f65d911ddc9ccfafb905
MIDNIGHT2400 <zinyshen24@gmail.com>
2024-05-03 07:05:35 +0300

    update app.py

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
+from flask import Flask
+from routes import handle_index_request  # Імпорт функції для обробки запитів
 
-# test commits
+app = Flask(__name__)  # Створення екземпляра Flask додатка
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    """
+    Основний маршрут програми.
+    Приймає GET і POST запити на кореневий URL.
+    """
+    return handle_index_request()  # Виклик функції обробки індексного запиту
+
+
+if __name__ == '__main__':
+    app.run(debug=False) # Запуск програми Flask
